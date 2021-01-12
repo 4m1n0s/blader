939eb57a2efec85e81191743ef29f2a4b03c2075
iAmine <tihouati.amine@gmail.com>
2021-01-12 12:58:49 +0100

    Add logo component as an example.

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<div class="min-h-screen flex flex-col items-center justify-center bg-gray-50 text-gray-800">
-		<div class="text-6xl font-medium">{{ 'Blader' }}</div>
+		<x-logo />
 		<div class="mt-4 text-sm text-gray-500">
 			{{ 'HTML Generator using Laravel Blade templating engine' }}
 		</div>
